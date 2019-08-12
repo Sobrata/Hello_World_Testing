11ffbd0bbda3c78fd7229e7d02485da27d88a6f5
Sobrata <paulsp2k9@gmail.com>
2019-08-12 09:03:46 +0600

    Modified the Telephone.java file inside the First Branch

--- Telephone.java
+++ Telephone.java
@@ -1,5 +1,6 @@
 class Telephone{
     public static void main(String[] args){
         System.out.println("This is from Telephone.java file");
+        System.out.println("Hello World! This is from Telephone.java file");
     }
 }
